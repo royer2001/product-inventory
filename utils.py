ba1164335f0d150e68fd28176091e5180210ae41
Royer <30308583+royer2001@users.noreply.github.com>
2024-10-27 22:51:07 -0500

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,5 +1,6 @@
 import os
 
+# Definiendo colores
 RED = "\033[31m"
 GREEN = "\033[32m"
 MAGENTA = "\033[35m"
@@ -12,15 +13,16 @@ def limpiar_pantalla():
     else:
         os.system("clear")
 
-
+# Función en desuso
 def generar_clave(diccionario):
     if diccionario:
         return max(diccionario.keys()) + 1
     else:
         return 1
 
-
+# Mostrar mensajes dependiendo a la opción ingresada
 def mostrar_mensaje(opcion):
+    # Diccionario de errores
     mensajes = {
         1: f"{GREEN}*** Agregado exitosamente ***",
         2: f"{RED}*** Eliminado exitosamente ***",
